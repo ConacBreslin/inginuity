549b8679f819a9493a819f8f852d400ac27ef9cc
ConacBreslin <conacbreslin@sky.com>
2021-11-07 18:08:27 +0000

    Add shoppingbag context and free delivery logic

--- inginuity/settings.py
+++ inginuity/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'shoppingbag.contexts.shoppingbag_contents',
             ],
         },
     },
@@ -161,3 +162,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
