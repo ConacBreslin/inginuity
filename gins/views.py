4dd81352ad5fd721460ac107d603a048ed4bba86
ConacBreslin <conacbreslin@sky.com>
2021-11-07 15:48:20 +0000

    Add search functionality

--- gins/views.py
+++ gins/views.py
@@ -1,22 +1,56 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Gin
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+from .models import Gin, Distillery
 
-# Create your views here.
 
 def all_gins(request):
-    """ A view to show all tyhe gins and sorting and search queries """
+    """ A view to show all gins, including sorting and search queries """
 
     gins = Gin.objects.all()
+    query = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                gins = gins.annotate(lower_name=Lower('name'))
+            if sortkey == 'distillery':
+                sortkey = 'distillery__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            gins = gins.order_by(sortkey)
+
+            """I haven't used the category filtering used in Boutique ADO""" 
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('gins'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            gins = gins.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'gins': gins,
+        'search_term': query,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'gins/gins.html', context)
 
 
 def individual_gin(request, gin_id):
-    """ A view to show individual gin details """
+    """ A view to show individual gins """
 
     gin = get_object_or_404(Gin, pk=gin_id)
 
