860b5ef8d2fba417c38f0f6b2d0ec4ea99f04408
ConacBreslin <conacbreslin@sky.com>
2021-11-07 14:28:38 +0000

    Create and register gin and distillery models

---
+++ gins/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Gin, Distillery
+
+# Register your models here.
+admin.site.register(Gin)
+admin.site.register(Distillery)
---
+++ gins/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+class Distillery(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Distilleries'
+    
+    name = models.CharField(max_length=100)
+    town = models.CharField(max_length=50, null=True, blank=True)
+    county = models.CharField(max_length=10)
+    province = models.CharField(max_length=10)
+    description = models.TextField(null=True, blank=True)
+    hasvisitorcetre = models.BooleanField()
+    rating = models.DecimalField(max_digits=4, decimal_places=1, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Gin(models.Model):
+    name = models.CharField(max_length=254)
+    distillery = models.ForeignKey('Distillery', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
