411025e0312fc47e22447044dda7932ca1adc2fc
ConacBreslin <conacbreslin@sky.com>
2021-11-07 14:36:57 +0000

    Customise Admin display

--- gins/admin.py
+++ gins/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from .models import Gin, Distillery
 
+
+class DistilleryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'county',
+        'province',
+        'image',
+    )
+
+    ordering = ('county',)
+
+
+class GinAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('price',)
+
+
 # Register your models here.
-admin.site.register(Gin)
-admin.site.register(Distillery)
+
+admin.site.register(Distillery, DistilleryAdmin)
+admin.site.register(Gin, GinAdmin)
--- gins/models.py
+++ gins/models.py
@@ -12,7 +12,6 @@ class Distillery(models.Model):
     province = models.CharField(max_length=10)
     description = models.TextField(null=True, blank=True)
     hasvisitorcetre = models.BooleanField()
-    rating = models.DecimalField(max_digits=4, decimal_places=1, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
