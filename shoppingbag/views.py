67039fa0b986573f9868fe9d4b395bc267881f9a
ConacBreslin <conacbreslin@sky.com>
2021-11-14 13:43:24 +0000

    Add functionality to update and remove items from bag

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -23,3 +23,33 @@ def add_to_shoppingbag(request, item_id):
 
     request.session['shoppingbag'] = shoppingbag
     return redirect(redirect_url)
+
+
+def adjust_shoppingbag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    shoppingbag = request.session.get('shoppingbag', {})
+    
+    if quantity > 0:
+        shoppingbag[item_id] = quantity
+    else:
+        shoppingbag.pop(item_id)
+
+    request.session['shoppingbag'] = shoppingbag
+    return redirect(reverse('view_shoppingbag'))
+
+
+def remove_from_shoppingbag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        shoppingbag = request.session.get('shoppingbag', {})
+            
+        shoppingbag.pop(item_id)
+
+        request.session['shoppingbag'] = shoppingbag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
