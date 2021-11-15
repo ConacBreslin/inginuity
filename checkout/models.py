d5c4ecb995d70b089fd26fdc9ffb313887873498
ConacBreslin <conacbreslin@sky.com>
2021-11-15 10:03:59 +0000

    Create checkout app and models

---
+++ checkout/models.py
@@ -0,0 +1,60 @@
+import uuid
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+from gins.models import Gin
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        """Private method to generate a unique order number"""
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """ To update grand total when a line item is added, incl delivery costs"""
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """To override the save method and set the order number if it hasn't already been set."""
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    gin = models.ForeignKey(Gin, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """To override the save method and set the lineitem total and update the order total."""
+        self.lineitem_total = self.gin.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Order Number {self.order.order_number}'
--- inginuity/settings.py
+++ inginuity/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'home',
     'gins',
     'shoppingbag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
