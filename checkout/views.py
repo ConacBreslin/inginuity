cf97803c5055a7708d6374f1377fc2d9c76b8987
ConacBreslin <conacbreslin@sky.com>
2021-11-16 13:45:02 +0000

    create functionality to create database objects in webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404. HttpResponse
 from django.contrib import messages
-from .forms import OrderForm, Order
-from shoppingbag.contexts import shoppingbag_contents
+from django.views.decorators.http import require_POST
 from django.conf import settings
-from gins.models import Gin
+from .forms import OrderForm, Order
 from .models import OrderLineItem, Order
+from gins.models import Gin
+from shoppingbag.contexts import shoppingbag_contents
 
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'shoppingbag': json.dumps(request.session.get('shoppingbag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, we were unable to \
+            processyour request. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
