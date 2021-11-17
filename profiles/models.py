7184e8e3c07704e5451602fa07ed9b6da0eb3e95
ConacBreslin <conacbreslin@sky.com>
2021-11-17 11:59:28 +0000

    Style allauth templates

--- profiles/models.py
+++ profiles/models.py
@@ -26,7 +26,7 @@ class UserProfile(models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """Tp create or update the user profile
     """
-    #if created:
-    UserProfile.objects.create(user=instance)
+    if created:
+        UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
-    #instance.userprofile.save()
+    instance.userprofile.save()
