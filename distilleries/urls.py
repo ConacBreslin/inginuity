2c8d32fd6ee49f0312fa9c2b49282e8934d173d2
ConacBreslin <conacbreslin@sky.com>
2021-11-23 11:57:26 +0000

    Add forma and functionality to add distilleries

--- distilleries/urls.py
+++ distilleries/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_distilleries, name='distilleries'),
-    path('<distillery_id>', views.individual_distillery, name='individual_distillery'),
+    path('<int:distillery_id>/', views.individual_distillery, name='individual_distillery'),
+    path('add/', views.add_distillery, name='add_distillery'),
 ]
--- distilleries/views.py
+++ distilleries/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import render
-from gins.models import Distillery
-
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from gins.models import Gin, Distillery
+from .forms import DistilleryForm
 
 
 def all_distilleries(request):
-    """The view to show all distilleries, including sorting and search queries """
+    """The view to show all distilleries"""
 
     distilleries = Distillery.objects.all()
 
@@ -28,4 +29,27 @@ def individual_distillery(request, distillery_id):
     return render(request, 'distilleries/individual_distillery.html', context)
 
 
-   
+@login_required
+def add_distillery(request):
+    """The view to add a distillery to the site"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only approved users can do this.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = DistilleryForm(request.POST, request.FILES)
+        if form.is_valid():
+            distillery= form.save()
+            messages.success(request, 'You added a new distillery!')
+            return redirect(reverse('individual_distillery', args=[gin.id]))
+        else:
+            messages.error(request, 'This distillery failed to add. Please check the form is valid.')
+    else:
+        form = DistilleryForm()
+        
+    template = 'distilleries/add_distillery.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
