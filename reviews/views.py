506ecab524d53db3c1159ea395d1fa60f54a9c94
ConacBreslin <conacbreslin@sky.com>
2021-11-25 09:30:00 +0000

    Add template for all reviews

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from gins.models import Distillery
 from.models import Review
+from django.db.models import Q
 
 
 
@@ -10,9 +11,22 @@ def all_reviews(request):
     """The view to show all reviews for any one distillery"""
 
     reviews = Review.objects.all()
+    thisquery = None
+
+    if request.GET:
+        if 'qq' in request.GET:
+            thisquery = request.GET['qq']
+            if not thisquery:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('reviews'))
+            
+            thesequeries = Q(distillery__icontains=thisquery) | Q(body__icontains=thisquery)
+            reviews = reviews.filter(thesequeries)
+
 
     context = {
         'reviews': reviews,
+        'search_term': thisquery,
     }
 
     return render(request, 'reviews/reviews.html', context)
