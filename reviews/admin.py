17211d4806a1fafd17953bba7a81bea197ddf92c
ConacBreslin <conacbreslin@sky.com>
2021-11-29 11:20:23 +0000

    I think reviews is messed up

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
 from .models import Review
 from gins.models import Distillery
+from django.contrib.auth.models import User
 
 
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = (
-        'author',
-        'distillery',
-        'first_created_on',
-        'last_editted_on'
-    )
 
 
-admin.site.register(Review, ReviewAdmin)
+
+admin.site.register(Review)
+
--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 from gins.models import Distillery
-from profiles.models import UserProfile
 
 
 class Review(models.Model):
-    author = models.OneToOneField(User, on_delete=models.CASCADE),
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     distillery = models.ForeignKey(Distillery, on_delete=models.CASCADE)
     body = models.CharField(max_length=1000)
     first_created_on = models.DateTimeField(auto_now_add=True)
@@ -13,3 +13,4 @@ class Review(models.Model):
 
     def __str__(self):
         return str(self.distillery)
+
