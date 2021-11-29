71ae2904a40f7b781b708cbfbfd5b6e80253d030
ConacBreslin <conacbreslin@sky.com>
2021-11-29 18:40:09 +0000

    Add reviews to whole site search

--- wholesitesearch/views.py
+++ wholesitesearch/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from gins.models import Gin, Distillery
+from reviews.models import Review
+from django.contrib.auth.models import User
 
 
 def wholesite_search(request):
@@ -10,11 +12,13 @@ def wholesite_search(request):
     if not q:
         messages.error(request, "You didn't enter any search criteria!")
         return redirect(reverse('home'))
-    distilleries = Distillery.objects.filter( Q(name__icontains=q) | Q(description__icontains=q) | Q(county__icontains=q))
+    distilleries = Distillery.objects.filter( Q(name__icontains=q) | Q(description__icontains=q) | Q(county__icontains=q) | Q(province__icontains=q))
     gins = Gin.objects.filter( Q(name__icontains=q) | Q(description__icontains=q) )
+    reviews = Review.objects.filter(body__icontains=q)
     context = {
             'distilleries': distilleries,
             'gins': gins,
+            'reviews': reviews,
         }
 
     return render(request, 'wholesitesearch/wholesitesearch.html', context)
