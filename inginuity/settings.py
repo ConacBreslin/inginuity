e4413a608a9df34ac10968a6e2464bc347b88da1
ConacBreslin <conacbreslin@sky.com>
2021-12-06 08:30:52 +0000

    Correct path route to logo and favicon in deployment

--- inginuity/settings.py
+++ inginuity/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     'distilleries',
     'reviews',
     'wholesitesearch'
-    
+
 ]
 
 MIDDLEWARE = [
@@ -184,7 +184,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'inginuity'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -201,7 +201,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
 
 
 # Default primary key field type
