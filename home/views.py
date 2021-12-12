ad0f3d38da3ef0cb1cf4b2bf666808201bb85222
ConacBreslin <conacbreslin@sky.com>
2021-12-12 10:49:26 +0000

    Add gen info page

--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ def index(request):
     """The view to render the index page """
 
     return render(request, 'home/index.html')
+
+
+def geninfo(request):
+    """The view to render the geninfo page """
+
+    return render(request, 'home/geninfo.html')
